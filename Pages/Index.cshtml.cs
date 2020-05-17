5190ddaa61e5139ff3ab0494c414f9abd8b4de1e
Julian Chu <yulang.chu@gmail.com>
2020-05-17 12:59:16 +0200

    add ArticlesDto to index page, remove hardcoded html

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,6 +11,7 @@ namespace SkillTreeRazorPageBlogSample.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<ArticleDto> Articles = new List<ArticleDto>();
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +20,25 @@ namespace SkillTreeRazorPageBlogSample.Pages
 
         public void OnGet()
         {
-
+         Articles.Add(new ArticleDto()
+         {
+             Id =  Guid.NewGuid(),
+             Title = "Test Title",
+             Content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+             CoverImage = "http://placehold.it/750x300",
+             Tags = "cloud,azure,dotnetcore",
+             CreatedAt = DateTime.Now
+         }); 
         }
+    }
+
+    public class ArticleDto
+    {
+        public string CoverImage { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string  Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Tags { get; set; }
     }
 }
