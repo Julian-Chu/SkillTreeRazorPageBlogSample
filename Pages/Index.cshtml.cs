48194c697dff02ad9d2e587a507c07e9e02c8d52
Julian Chu <yulang.chu@gmail.com>
2020-05-17 15:45:45 +0200

    data binding

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,39 +5,49 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SkillTreeRazorPageBlogSample.Data;
 
 namespace SkillTreeRazorPageBlogSample.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        public List<ArticleDto> Articles = new List<ArticleDto>();
+        private readonly RazorPageBlogContext _context;
+        public IList<ArticleDto> Articles { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, RazorPageBlogContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
-         Articles.Add(new ArticleDto()
-         {
-             Id =  Guid.NewGuid(),
-             Title = "Test Title",
-             Content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
-             CoverImage = "http://placehold.it/750x300",
-             Tags = "cloud,azure,dotnetcore",
-             CreatedAt = DateTime.Now
-         }); 
+            Articles = _context.Articles.Select(a => new ArticleDto()
+            {
+                Id = a.Id,
+                Content = a.Body,
+                Tags = a.Tags,
+                Title = a.Title,
+                CoverImage = a.CoverPhoto,
+                CreatedAt = a.CreateDate
+            }).ToList();
         }
     }
 
     public class ArticleDto
     {
+        private string _content;
         public string CoverImage { get; set; }
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string  Content { get; set; }
+
+        public string Content
+        {
+            get => _content.Substring(0,20);
+            set => _content = value;
+        }
+
         public DateTime CreatedAt { get; set; }
         public string Tags { get; set; }
     }
