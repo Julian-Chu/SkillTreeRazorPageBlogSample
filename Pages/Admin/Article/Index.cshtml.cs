c6d5a84235e534dc1de2dde6a58a64c07d1f8724
Julian Chu <yulang.chu@gmail.com>
2020-05-18 23:25:38 +0200

    add admin scaffold

---
+++ Pages/Admin/Article/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SkillTreeRazorPageBlogSample.Data;
+
+namespace SkillTreeRazorPageBlogSample.Pages.Admin.Article
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SkillTreeRazorPageBlogSample.Data.RazorPageBlogContext _context;
+
+        public IndexModel(SkillTreeRazorPageBlogSample.Data.RazorPageBlogContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Articles> Articles { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Articles = await _context.Articles.ToListAsync();
+        }
+    }
+}
