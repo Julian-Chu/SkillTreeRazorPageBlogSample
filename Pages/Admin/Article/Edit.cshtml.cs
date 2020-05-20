834fe8ef36eff21ce24c38d7a495ce3430108df0
Julian Chu <yulang.chu@gmail.com>
2020-05-20 22:41:38 +0200

    implement edit page

--- Pages/Admin/Article/Edit.cshtml.cs
+++ Pages/Admin/Article/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -19,8 +20,12 @@ namespace SkillTreeRazorPageBlogSample.Pages.Admin.Article
             _context = context;
         }
 
-        [BindProperty]
-        public Articles Articles { get; set; }
+        [BindProperty] public Articles Articles { get; set; }
+
+
+        [BindProperty] public IFormFile CoverPhoto { get; set; }
+        [BindProperty] public IEnumerable<SelectListItem> Tags { get; set; }
+
 
         public async Task<IActionResult> OnGetAsync(Guid? id)
         {
@@ -30,18 +35,31 @@ namespace SkillTreeRazorPageBlogSample.Pages.Admin.Article
             }
 
             Articles = await _context.Articles.FirstOrDefaultAsync(m => m.Id == id);
+            Tags = await _context.TagCloud.Select(tag => new SelectListItem() {Text = tag.Name, Value = tag.Name})
+                .ToListAsync();
+            var defaultTags = Articles.Tags.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(t => new SelectListItem() {Text = t, Value = t}).ToList();
+            foreach (var item in Tags)
+            {
+                if (defaultTags.Any(t => t.Value == item.Value))
+                {
+                    item.Selected = true;
+                }
+            }
 
             if (Articles == null)
             {
                 return NotFound();
             }
+
             return Page();
         }
 
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(IFormFile coverPhoto, IEnumerable<string> tags)
         {
+            ModelState.Remove("Articles.CoverPhoto");
             if (!ModelState.IsValid)
             {
                 return Page();
@@ -51,6 +69,44 @@ namespace SkillTreeRazorPageBlogSample.Pages.Admin.Article
 
             try
             {
+                Articles.CoverPhoto = $"http://placehold.it/750x300?text=null";
+                if (coverPhoto != null)
+                {
+                    Articles.CoverPhoto = $"http://placehold.it/750x300?text={coverPhoto.FileName}";
+                }
+
+                var prevTags = string.IsNullOrEmpty(Articles.Tags)?new List<string>():Articles.Tags.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList();
+                var unchangedTags = prevTags.Intersect(tags);
+                var tagsToAdd = tags.Except(unchangedTags);
+                var tagsToRemove = prevTags.Except(unchangedTags);
+                foreach (var tag in tagsToAdd)
+                {
+                    var tagCloud = _context.TagCloud.SingleOrDefault(t => t.Name == tag);
+                    if (tagCloud != null)
+                    {
+                        tagCloud.Amount++;
+                        _context.TagCloud.Update(tagCloud);
+                    }
+                    else
+                    {
+                        _context.TagCloud.Add(new TagCloud() {Id = Guid.NewGuid(), Amount = 1, Name = tag});
+                    }
+                }
+
+                foreach (var tag in tagsToRemove)
+                {
+                    var tagCloud = _context.TagCloud.SingleOrDefault(t => t.Name == tag);
+                    if (tagCloud != null)
+                    {
+                        if (tagCloud.Amount >= 1)
+                        {
+                            tagCloud.Amount--;
+                            _context.TagCloud.Update(tagCloud);
+                        }
+                    }
+                }
+
+                Articles.Tags = string.Join(",", tags);
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
